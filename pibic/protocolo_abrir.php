92419c90b966f3124652c4dd44b85f1251dd0ac4
ElizandroLima <elizandro.slima@gmail.com>
2015-03-05 11:15:32 -0300

    Alteração font [protocolo_abrir.php]
    Adicionado texto pré definidos para cada opção de solicitação no perfil
    do professor.

--- pibic/protocolo_abrir.php
+++ pibic/protocolo_abrir.php
@@ -61,17 +61,66 @@ while ($line = db_read($rlt))
 echo '<table width="100%" class="tabela00" border=0 >'.$sx.'</table>';
 echo '</form>';
 echo '<BR><BR>';
-echo '<p align="left">	Prezado Professor Orientador, </p>
-						<p>Esclarecemos que todas as substituições de projetos com BOLSA deverão ser solicitadas 
-						até o dia 05 de cada mês para que o aluno vigente receba referente ao mês solicitado.
-						Caso contrário a bolsa será paga no mês seguinte à solicitação. </p>
-						<p>Cancelamentos de bolsas serão aceitos somente até 30 de abril de 2015, com justificativa,
-						sendo analisada pelo Comitê Gestor a possibilidade de devolução dos valores recebidos. O professor 
-						orientador deverá entregar as atividades previstas neste termo e será analisada a motivação do 
-						cancelamento, podendo ou não ocorrer penalidade na próxima seleção. Após 1º de maio todos os projetos 
-						vigentes deverão participar obrigatoriamente do SEMIC, conforme o item 3.2. Compromissos e Direitos do 
-						professor orientador, letra x.
-	  </p>';
+
+/*
+ * Seta o perfil com a opção escolhida, para cada aopção existe um texto diferente
+ * 	ALT = Alteração de título do Plano do Aluno
+ * 	SBS = Substituição do aluno
+ * 	CAN = Cancelamento de orientação
+ */
+if ($dd[1] == 'SBS')
+	{
+		echo '	<p align="justify" style="width:50%; line-height:150%;"> Prezado Professor Orientador, </p>
+		
+				<p align="justify" style="width:50%; line-height:150%;"> Esclarecemos que todas as substituições de projetos com BOLSA deverão ser solicitadas 
+				até o dia 05 de cada mês para que o aluno vigente receba referente ao mês solicitado.
+				Caso contrário a bolsa será paga no mês seguinte à solicitação. </p>
+				
+				<p align="justify" style="width:50%; line-height:150%;"> Cancelamentos de bolsas serão aceitos somente até 30 de abril de 2015, com justificativa,
+				sendo analisada pelo Comitê Gestor a possibilidade de devolução dos valores recebidos. O professor 
+				orientador deverá entregar as atividades previstas neste termo e será analisada a motivação do 
+				cancelamento, podendo ou não ocorrer penalidade na próxima seleção. Após 1º de maio todos os projetos 
+				vigentes deverão participar obrigatoriamente do SEMIC, conforme o item 3.2. Compromissos e Direitos do 
+				professor orientador, letra x.</p>';
+		
+	} elseif ($dd[1] == 'CAN')
+		{
+			echo '	<p align="justify" style="width:50%; line-height:150%;">Prezado Professor Orientador, </p>
+			
+					<p align="justify" style="width:50%; line-height:150%;"> Cancelamentos de bolsas serão aceitos somente até 30 de abril de 2015, com justificativa,
+					sendo analisada pelo Comitê Gestor a possibilidade de devolução dos valores recebidos. O professor 
+					orientador deverá entregar as atividades previstas neste termo e será analisada a motivação do 
+					cancelamento, podendo ou não ocorrer penalidade na próxima seleção. Após 1º de maio todos os projetos 
+					vigentes deverão participar obrigatoriamente do SEMIC, conforme o item 3.2. Compromissos e Direitos do 
+					professor orientador, letra x.</p>';
+			
+		}elseif ($dd[1] == 'ALT')
+			{
+			  echo'<p align="justify" style="width:50%; line-height:150%;"> Prezado Professor Orientador, </p>
+				
+					<p align="justify" style="width:50%; line-height:150%;"> Para alteração de título do projeto solicitamos sua <B>especial atenção</B> nos seguintes procedimentos abaixo:
+						<p align="justify" style="width:50%; line-height:150%;">
+							<ul>
+								<li style="list-style-type: disc; font-family: arial, sans-serif; font-size: 12px; margin-top:5px; line-height:120%;"> 
+									Todos os títulos dos projetos devem estar padronizados: Primeira letra maiúscula e as demais minúsculas, exceto 
+									para nome próprio;</li>
+								<li style="list-style-type: disc; font-family: arial, sans-serif; font-size: 12px; margin-top:5px; line-height:120%;"> 
+									Caso tenha ocorrido algum problema no desenvolvimento do projeto que <B>justifique</B> alguma alteração de título fique 
+									atento que a mudança deve respeitar o proposto no projeto aprovado no edital. <B>Bolsas CNPq e Fundação Araucária não 
+									autorizam</B> esse ajuste, neste caso o projeto será transformado em ICV ou ITV;</li>
+							</ul>
+						</p>
+	  				</p>';
+		
+			}
+
+
+
+
+
+
+
+
 
 
 echo '<BR><BR><BR><BR><BR><BR><BR>';
