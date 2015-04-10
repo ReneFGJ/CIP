dd232bc926939a44ed606899aeed9a77a9534294
Rene FGJr <renefgj@hotmail.com>
2015-04-10 12:57:56 -0300

    v0.15.15c
    - Mascara do numeral

--- cip/bonificacao_ed.php
+++ cip/bonificacao_ed.php
@@ -6,19 +6,29 @@ $LANG = 'pt_BR';
 $file = '../messages/msg_'.$LANG.'.php';
 $jid = 0;
 ?><head>
-	<title>::CIP::</title>
-	<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-	<link rel="shortcut icon" type="image/x-icon" href="http://cip.pucpr.br/favicon.ico" />
-	<link rel="STYLESHEET" type="text/css" href="../css/letras.css">
-	<script  type="text/javascript" src="../js/jquery.js"></script>
-	<script  type="text/javascript" src="../js/jquery.corner.js"></script>
-	<script  type="text/javascript" src="../js/jquery.example.js"></script>
-</head><?
+<META HTTP-EQUIV=Refresh CONTENT="3600; URL=http://www2.pucpr.br/reol/logout.php">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /><meta name="description" content="">
+<link rel="shortcut icon" type="image/x-icon" href="http://www2.pucpr.br/reol/favicon.ico" />
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_cabecalho.css">
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_midias.css">
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_body.css">
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_fonts.css">
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_botao.css">
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_table.css">
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_font_roboto.css">
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_font-awesome.css">
+<link rel="stylesheet" href="http://www2.pucpr.br/reol/css/style_form.css">
+<script language="JavaScript" type="text/javascript" src="http://www2.pucpr.br/reol/js/jquery-1.7.1.js"></script>
+<script language="JavaScript" type="text/javascript" src="http://www2.pucpr.br/reol/js/jquery.corner.js"></script>
+<script language="JavaScript" type="text/javascript" src="http://www2.pucpr.br/reol/js/jquery-ui.js"></script>
+<title>CIP | Centro Integrado de Pesquisa | PUCPR</title>
+</head>
+<?
 
 require($include.'sisdoc_debug.php');
 require($include.'sisdoc_data.php');
 
-require($include.'_class_form.php');
+require('../_include/_class_form.php');
 $form = new form;
 
 /* Segurança */
@@ -71,6 +81,4 @@ if ($form->saved > 0)
 	} else {
 		echo $tela;
 	}
-
-
 ?>
