ff41c0c3f9db85b6b6f5caa6d255439997781672
ElizandroLima <elizandro.slima@gmail.com>
2015-05-12 14:37:42 -0300

    #Atualização 11.05.2015.b
    Criação do rel para Ciências e trascendência

--- pibicpr/submissao_areas.php
+++ pibicpr/submissao_areas.php
@@ -14,16 +14,20 @@ echo '<H3>Projetos por áreas estratégicas</h3>';
 $cp = array();
 $sql = "select a_cnpq || ' ' || a_descricao as a_descricao, a_cnpq from ajax_areadoconhecimento where a_semic = 1 order by a_cnpq";
 
-array_push($cp,array('$H8','','',False,False));
+
 array_push($cp,array('$Q a_descricao:a_cnpq:'.$sql,'','',False,False));
 //monta combo de anos com inicio em 1990 atï¿½ anobase atual
-array_push($cp, array('$[1990-' . date("Y") . ']', '', 'Escolha o ano para busca', False, False,''));
+array_push($cp, array('$[1990-' . date("Y") . ']', '', 'Escolha o ano para busca', False, True, ''));
+array_push($cp,array('$H8','','',False,False));
+
+//Captura ano selecionado e armazena na variavel $dd[0]
+if (strlen($dd[0]) == 0) {$dd[0] = (date("Y")-1);}
 
 echo $form->editar($cp,'');
 
 if ($form->saved > 0)
 	{
-		echo $pj->projetos_seleciona_area($dd[1],date("Y"));
+		echo $pj->projetos_seleciona_area($dd[0],$dd[1]);
 		echo '<BR><BR>';
 	}
 ?>
