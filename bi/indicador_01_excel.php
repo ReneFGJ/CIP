a0adaf206800b2503304bf08b55a45f414028c3b
Rene FGJr <renefgj@hotmail.com>
2015-05-15 12:02:43 -0300

    v0.15.20e

--- bi/indicador_01_excel.php
+++ bi/indicador_01_excel.php
@@ -10,35 +10,46 @@ $bi = new bi;
 $bi->ano_ini = $dd[1];
 $bi->ano_fim = $dd[1];
 
-header("Content-Type: application/vnd.ms-excel; charset=utf-8");
-header("Content-type: application/x-msexcel; charset=utf-8");
+//header("Content-Type: application/vnd.ms-excel; charset=utf-8");
+
+//header("Content-type: application/x-msexcel; charset=iso-8859-1");
+//header("Content-type: application/x-msexcel; charset=utf-8");
+header("Content-type: application/vnd.ms-excel; charset=iso-8859-1");
 header("Content-Disposition: attachment; filename=estatistica_".date("Y-m-d").".xls"); 
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Pragma: public");
 header("Expires: 0");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header("Cache-Control: private",false);
+header("Content-Type: application/force-download");
+header("Content-Type: application/octet-stream");
+header("Content-Type: application/download");;
+header("Content-Transfer-Encoding: binary");
+echo '<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>';
 
 /* Professores com projetos de captacao aprovados e andamento
  * Campus, escola, programa
  */
-echo utf8_encode($bi->professor_com_captacao());
+echo trim($bi->professor_com_captacao());
 
  /* Captacao de Recursos via Agência d Fomento e Governamentais
   * Campus, escola, programa
   */
-echo utf8_encode($bi->professor_com_captacao_ag_gov());
+echo trim($bi->professor_com_captacao_ag_gov());
 
  /* Mobilidade Internacional Docente - SS
   * Campus, escola, programa
   */
-echo utf8_encode($bi->mobilidade_docente()); 
+echo trim($bi->mobilidade_docente()); 
 
  /* Mobilidade Internacional Discente - SS 
   * Campus, escola, programa
   */
-echo utf8_encode($bi->mobilidade_discente());
+echo trim($bi->mobilidade_discente());
  /* Nível de produção Qualificada - PPG
   * Campus, escola, programa
   */
-echo utf8_encode($bi->producao_docente());
+echo trim($bi->producao_docente());
 
 ?>
