9b22577e3555b23d2816dfbd861796c0eaf19966
ElizandroLima <elizandro.slima@gmail.com>
2015-06-23 13:32:26 -0300

    At.23.06.2015.a
    Fotos do SWB no Csf

--- cienciasemfronteiras/eventos.php
+++ cienciasemfronteiras/eventos.php
@@ -50,22 +50,22 @@
           </div>
 
           <br />
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+          
+			<!--
+          <p>
+          	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			-->
 
           <div class="fotos-header">
-            <img src="img/fotos-swb-1.jpg" style="width: 100%;" />
+            <img src="img/fotos-swb-1a.jpg" style="width: 100%;" />
           </div>
-
-
-
+		
           <div class="fotos-header">
             <img src="img/fotos-swb-2.jpg" style="width: 100%;" />
           </div>
 
           <div class="fotos-header">
-            <img src="img/fotos-swb-3.jpg" style="width: 100%;" />
+           <!-- <img src="img/fotos-swb-3.jpg" style="width: 100%;" /> -->
           </div>
 
           
