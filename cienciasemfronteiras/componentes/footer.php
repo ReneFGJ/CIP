0c16a12f71de77ecbd313b79275bd8d017c26c63
Rene FGJr <renefgj@hotmail.com>
2015-06-29 11:24:05 -0300

    v0.15.18a

--- cienciasemfronteiras/componentes/footer.php
+++ cienciasemfronteiras/componentes/footer.php
@@ -3,9 +3,8 @@
         <p class="pull-right"><a href="#">Voltar para o topo</a></p>
         <p><strong>Pontifícia Universidade Católica do Paraná</strong></p>
         <br />
-        <p>Coordenação de Iniciação Científica<br />
+        <p>Coordenação do Ciência sem Fronteiras da PUCPR<br />
         (41) 3271-2112 | 3271-1602<br />
-        	pibicpr@pucpr.br
-        </p>
+        	csf@pucpr.br
       </footer>
       
