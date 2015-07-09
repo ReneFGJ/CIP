530c32d33f015901c6a3f405449cdc956e77e3eb
ElizandroLima <elizandro.slima@gmail.com>
2015-07-09 17:51:50 -0300

    At.09.07.2015.b

--- cnpq/view/apresentacao_origem_bolsas.php
+++ cnpq/view/apresentacao_origem_bolsas.php
@@ -1,102 +1,96 @@
 <?php
-/* PIBIC */
-$dado1 = array('CNPq' => 94, 'FA' => 145, 'PUCPR' => 350, 'ICV' => 448);
-$title = 'Origem das bolsas PIBIC';
-$title2 = '';
-$ybar = 'Distribuição';
-
-/* PIBITI  model: pizza */
+/* PIBIC  model: pizza */
 $dado = array('CNPq' => 94, 'FA' => 145, 'PUCPR' => 350, 'ICV' => 448);
 $title = 'Origem das bolsas PIBIC';
 $title2 = '';
 $ybar = 'Distribuição';
-?>
+$div_name = 'pibic';
 
+/* PIBITI */
+/*
+$dado = array('CNPq' => 94, 'FA' => 145, 'PUCPR' => 350, 'ICV' => 448);
+$title = 'Origem das bolsas PIBITI';
+$title2 = '';
+$ybar = 'Distribuição';
+*/
+
+$dados = '';
+foreach ($dado as $key => $value) {
+	$dados  .= "['$key',   $value],";
+}
+
+?>
 <!-- Grafico HighChart--->
 <?
-foreach ($dado as $value) {
-    $value;
-?>
-	<body>
-		<?
-		$sx .= '
-				<!DOCTYPE HTML>
-				<html>
-					<head>
-						<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-						<title>Highcharts Example</title>
-				
-						<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-						<style type="text/css">
-				${demo.css}
-						</style>
-						<script type="text/javascript">
-				$(function () {
-				
-				    // Radialize the colors
-				    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
-				        return {
-				            radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
-				            stops: [
-				                [0, color],
-				                [1, Highcharts.Color(color).brighten(-0.3).get(\'rgb\')] // darken
-				            ]
-				        };
-				    });
-				
-				    // Build the chart
-				    $(\'#container\').highcharts({
-				        chart: {
-				            plotBackgroundColor: null,
-				            plotBorderWidth: null,
-				            plotShadow: false
-				        },
-				        title: {
-				            text: \'Grafico\'
-				        },
-				        tooltip: {
-				            pointFormat: \'{series.name}: <b>{point.percentage:.1f}%</b>\'
-				        },
-				        plotOptions: {
-				            pie: {
-				                allowPointSelect: true,
-				                cursor: \'pointer\',
-				                dataLabels: {
-				                    enabled: true,
-				                    format: \'<b>{point.name}</b>: {point.percentage:.1f} %\',
-				                    style: {
-				                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || \'black\'
-				                    },
-				                    connectorColor: \'silver\'
-				                }
-				            }
-				        },
-				        series: [{
-				            type: \'pie\',
-				            name: \'Browser share\',
-				            data: [
-				                [[\'$value\']]
-				            ]
-				        }]
-				    });
-				});
-				
-				</script>
-					</head>
-					<body>
-						<script src="../../js/highcharts.js"></script>
-						<script src="../../js/modules/exporting.js"></script>
-						
-						<div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto">
-						</div>
-					</body>
-				</html>
-					
-		';
-		}
-		echo "$sx";
-														
-		?>
+$sx .= '
+		<!DOCTYPE HTML>
+		<html>
+			<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+				<title>'.$title.'</title>
 		
-	</body>
-</html>
+				<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+				<style type="text/css">${demo.css}</style>
+				
+				<!--Gera o grafico -->
+				<script type="text/javascript">
+					$(function () {
+					    // Radialize the colors
+					    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+					        return {
+					            radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+					            stops: [
+					                [0, color],
+					                [1, Highcharts.Color(color).brighten(-0.3).get(\'rgb\')] // darken
+					            ]
+					        };
+					    });
+					
+					    // Build the chart
+					    $(\'#'.$div_name.'\').highcharts({
+					        chart: {
+					            plotBackgroundColor: null,
+					            plotBorderWidth: null,
+					            plotShadow: false
+					        },
+					        title: {
+					            text: \''.$title.'\'
+					        },
+					        tooltip: {
+					            pointFormat: \'{series.name}: <b>{point.y} bolsas</b>\'
+					        },
+					        plotOptions: {
+					            pie: {
+					                allowPointSelect: true,
+					                cursor: \'pointer\',
+					                dataLabels: {
+							                    enabled: true,
+							                    format: \'<b>{point.name}</b>: {point.percentage:.1f} %\',
+							                    style: {
+							                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || \'black\'
+							                    },
+							                    connectorColor: \'silver\'
+					                }
+					            }
+					        },
+					        series: [{
+					            type: \'pie\',
+					            name: \'Quantidade\',
+					            data: ['.$dados.']
+					        }]
+					    });
+					});
+		</script>
+			</head>
+			<body>
+				<script src="../../js/highcharts.js"></script>
+				<script src="../../js/modules/exporting.js"></script>
+				<div id="'.$div_name.'" style="min-width: 310px; height: 400px; max-width: 600px; margin: 1px; auto"></div>
+			</body>
+		</html>
+			
+';
+
+echo "$sx";
+?>
+		
--- cnpq/view/apresentacao_semic.php
+++ cnpq/view/apresentacao_semic.php
@@ -5,14 +5,102 @@ $title = 'Histórico dos projetos finalizados';
 $title2 = 'Apresentação SEMIC - PIBIC';
 $ybar = 'Total de projetos';
 $xbar = 'Ano';
+$div_name = 'pibic';
 
 
 /* PIBITI model: bars*/
+/*
 $dado = array('2011'=>51,'2012'=>81,'2013'=>95,'2014'=>107,'2015'=>140);
 $title = 'Histórico dos projetos finalizados';
 $title2 = 'Apresentação SEMIC - PIBIC';
 $ybar = 'Total de projetos';
 $xbar = 'Ano';
+*/
+
+$dados = '';
+
+foreach ($dado as $key => $value) {
+		$dados  .= "['$key',   $value],";
+}
+
 ?>
 
 <!-- Grafico HighChart--->
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Highcharts Example</title>
+
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<style type="text/css">
+${demo.css}
+		</style>
+		<script type="text/javascript">
+$(function () {
+    $('#<?php  echo $div_name; ?>').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'World\'s largest cities per 2014'
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Population (millions)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Population in 2008: <b>{point.y:.1f} millions</b>'
+        },
+        series: [{
+            name: 'Population',
+            data: [
+               <?php echo "$dados"; ?>
+            ],
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+});
+		</script>
+	</head>
+	<body>
+<script src="../../js/highcharts.js"></script>
+<script src="../../js/modules/exporting.js"></script>
+
+<div id="<?php echo $div_name; ?>" style="min-width: 300px; height: 400px; margin: 0 auto"></div>
+
+	</body>
+</html>
+
+
+
+
