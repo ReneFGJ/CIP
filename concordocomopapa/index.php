7904831db67311b5db7fc513959a7c531d667d9f
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2015-08-07 07:13:23 -0300

    v0.15.32
    Papa

---
+++ concordocomopapa/index.php
@@ -0,0 +1,21 @@
+<?php
+require ("view/header/cab.php");
+?>
+<script>
+	$("#example, body").vegas({
+		slides : [{
+			src : "img/papa/img_001.jpg"
+		}, {
+			src : "img/papa/img_002.jpg"
+		}, {
+			src : "img/papa/img_003.jpg"
+		}, {
+			src : "img/papa/img_004.jpg"
+		}, {
+			src : "img/papa/img_005.jpg"
+		}]
+	}); 
+</script>
+<div id="example">
+	<img src="img/papa/txto.png">
+</div>
---
+++ concordocomopapa/view/header/cab.php
@@ -0,0 +1,17 @@
+<head>
+	<meta charset="iso-8859-1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Concurso - Enciclica do Papa</title>
+	<meta name="description" content="Concurso PUCPR sobre a Enciclica do para">
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="cleartype" content="on">
+
+	<link rel='stylesheet' href='css/style_enciclica.css' />
+	<link rel="stylesheet" href="js/vegas/vegas.min.css">
+
+	<script src="http://code.jquery.com/jquery.min.js"></script>
+	<script src="http://zeptojs.com/zepto.min.js"></script>
+	<script src="js/vegas/vegas.min.js"></script>
+
+</head>
+<body>
