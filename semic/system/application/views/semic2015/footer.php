9801cd6f2cc453b9294e31cc30fe381116435fce
Elizandro Santos de Lima <santos.elizandro@pucpr.br>
2015-09-17 10:55:01 -0300

    At.17.09.2015.a

--- semic/system/application/views/semic2015/footer.php
+++ semic/system/application/views/semic2015/footer.php
@@ -21,34 +21,43 @@
 				<a HREF="<?php echo base_url('index.php/semic/contact');?>" class="link_foot"><?php echo msg('contact_system');?></A>
 			</td>
 			<!--Div Apoio -->
-			<td width="20%" >
+			<td width="25%" >
 				<b><?php echo msg('apoio');?></b>
 				<br>
 				<br>
+				&nbsp;
 				<a href="http://cienciaefe.pucpr.br/" target="_new"> 
 					<img src="<?php echo base_url('img/semic2015/logo_cf.png');?>" height="40" border=0> </a>
+				<br />
+				<a href="http://www.solmarista.org.br/" target="_new"> 	
+					<img src="<?php echo base_url('img/semic2015/marista_rede.png');?>" height="50" border=0> </a>
+				&nbsp;&nbsp;
+				<a href="http://www.centrodedefesa.org.br/" target="_new"> 	
+					<img src="<?php echo base_url('img/semic2015/marista_defesa.png');?>" height="55" border=0> </a>
 			</td>
 			<!--Div Agências de fomento -->
 			<td width="25%">
 				<b><?php echo msg('colaboracao');?></b>
 				<br>
 				<br>
+				&nbsp;
 				<a href="http://www.cnpq.br/" target="_new"> 
 					<img src="<?php echo base_url('img/semic2015/logo_cnpq.png');?>" height="25" border=0> </a> 
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
 				<a href="http://www.fappr.pr.gov.br/" target="_new"> 
-					<img src="<?php echo base_url('img/semic2015/logo_fa.png');?>" height="40" border=0> </a>
+					<img src="<?php echo base_url('img/semic2015/logo_fa.png');?>" height="35" border=0> </a>
 			</td>
 			<!--Div IES -->
-			<td width="30%">
+			<td width="25%">
 				<b><?php echo msg('ies');?></b>
 				<br>
 				<br>
+				&nbsp;
 				<a href="http://www.pucpr.br//" target="_new"> 
 									<img src="<?php echo base_url('img/semic2015/logo_pucpr_2.png');?>" height="80" border=0> </a>
 				&nbsp;&nbsp;
 				<a href="http://www.senaipr.org.br/" target="_new"> 
-					<img src="<?php echo base_url('img/semic2015/logo_senai_small.jpg');?>" height="25" border="0" style="position:absolute;"> </a>
+					<img src="<?php echo base_url('img/semic2015/logo_senai_small.jpg');?>" height="20" border="0" style="position:absolute;"> </a>
 				
 			</td>
 		</tr>
