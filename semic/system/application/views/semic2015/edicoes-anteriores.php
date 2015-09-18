b1899c461093cbd95a1515312d04968a166d9667
Elizandro Santos de Lima <santos.elizandro@pucpr.br>
2015-09-18 10:29:17 -0300

    At.18.09.2015.a

--- semic/system/application/views/semic2015/edicoes-anteriores.php
+++ semic/system/application/views/semic2015/edicoes-anteriores.php
@@ -2,101 +2,147 @@
 	<div class="text-3">
 		<h1>Edições Anteriores</h1>
 		<br />
+		<!--SEMIC 2014 -->
 		<div class="item-evento-anterior">
-			<div class="foto-evento-anterior">
-				<div class="semic-2014 round-foto"></div>
+			<div class="foto-evento-anterior" >
+				<a href="http://www2.pucpr.br/reol/eventos/cicpg/" target="_blank">
+					<div class="semic-2014 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/eventos/cicpg/" blank="_blank"><h3>2014 - III Congresso Sul Brasileiro de Iniciação Científica e Pós-Graduação (04, 05 e 06 de novembro de 2014)</h3></a>
-				<p></p>
+				<a href="http://www2.pucpr.br/reol/eventos/cicpg/" target="_blank"><h3>2014 - III Congresso Sul Brasileiro de Iniciação Científica e Pós-Graduação (04, 05 e 06 de novembro de 2014)</h3></a>
+				<p>
+					O objetivo é promover o intercâmbio de conhecimento e práticas entre instituições,
+					pesquisadores e bolsistas de Iniciação Científica, colocando em pauta pesquisa e inovação. Além de elevar a valorização de alunos de graduação e Pós-Graduação, e divulgar pesquisas
+					desenvolvidas por eles sob a orientação de professores-pesquisadores e por fim estimular a adesão de alunos à atividade de investigação.
+				</p>
+				<p>
+					Simultaneamente, a Pontifícia Universidade Católica (PUCPR) estará realizando seu XXII Seminário de Iniciação Cientifica (SEMIC) e o
+					III Seminário de Iniciação Tecnológica e Inovação.
+				</p>
 			</div>
 		</div>
+		
+		<!--SEMIC 2013 -->
 		<div class="item-evento-anterior">
 			<div class="foto-evento-anterior">
-				<div class="semic-2013 round-foto"></div>
+				<a href="http://www2.pucpr.br/reol/semic2013/" target="_blank">
+					<div class="semic-2013 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/semic2013/" blank="_blank"><h3>2013 - XXI Seminário de Iniciação Científica, XIV Mostra de Pesquisa, I PIBITI (22, 23 e 24 de novembro de 2013)</h3></a>
-				<p></p>
+				<a href="http://www2.pucpr.br/reol/semic2013/" target="_blank"><h3>2013 - XXI Seminário de Iniciação Científica, XIV Mostra de Pesquisa, I PIBITI (22, 23 e 24 de novembro de 2013)</h3></a>
+				<p>
+					A Iniciação Científica é sempre um desafio e a preparação do Seminário Anual é o segundo desafio desse percurso junto aos alunos iniciantes no campo da ciência. A importância desse caminho é inegável na descoberta de novos talentos e temos registrado boas surpresas. Queremos que o evento de 2013 seja mais uma delas, pois afinal de contas atingimos a maioridade: 21 anos de PIBIC na PUCPR!
+				</p>
 			</div>
 		</div>
+		
+		<!--SEMIC 2012 -->
 		<div class="item-evento-anterior">
 			<div class="foto-evento-anterior">
-				<div class="semic-2012 round-foto"></div>
+				<a href="http://www2.pucpr.br/reol/semic2012/" target="_blank">
+					<div class="semic-2012 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/semic2012/" blank="_blank"><h3>2012 - XX Seminário de Iniciação Científica, XIV Mostra de Pesquisa, I PIBITI (6, 7 e 8 de novembro de 2012)</h3></a>
+				<a href="http://www2.pucpr.br/reol/semic2012/" target="_blank"><h3>2012 - XX Seminário de Iniciação Científica, XIV Mostra de Pesquisa, I PIBITI (6, 7 e 8 de novembro de 2012)</h3></a>
 				<p>
 					Nesta edição estamos com novidades alinhadas com os temas da internacionalização da Universidade e do desenvolvimento tecnológico e inovação.
 				</p>
 			</div>
 		</div>
+		
+		<!--SEMIC 2011 -->
 		<div class="item-evento-anterior">
 			<div class="foto-evento-anterior">
-				<div class="semic-2011 round-foto"></div>
+				<a href="http://www2.pucpr.br/reol/index.php/SEMIC19?dd99=contact" target="_blank">
+					<div class="semic-2011 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/index.php/SEMIC19?dd99=contact" blank="_blank"><h3>2011 - XIX Seminário de Iniciação Científica, XIII Mostra de Pesquisa, I PIBITI (25, 26 e 27 de outubro de 2011)</h3></a>
+				<a href="http://www2.pucpr.br/reol/index.php/SEMIC19?dd99=contact" target="_blank"><h3>2011 - XIX Seminário de Iniciação Científica, XIII Mostra de Pesquisa, I PIBITI (25, 26 e 27 de outubro de 2011)</h3></a>
 				<p>
 					A novidade desta edição é o I Seminário de Iniciação em Desenvolvimento Tecnológico e Inovação (PIBITI) com 54 trabalhos. Somados a estes contamos com 461 pesquisas do PIBIC e 218 pesquisas de mestrado e doutorados dos diversos programas de pós-graduação stricto sensu da PUCPR, totalizando 679 trabalhos apresentados durante o evento. Este é sem dúvida, o ambiente adequado para a discussões de ideias inovadoras, para o compartilhar dos resultados das pesquisas com seus pares e, ainda, o espaço para os jovens pesquisadores receberem o feedback de pesquisadores experientes que atuam na qualidade de avaliadores. Anualmente a PUCPR premia os melhores trabalhos, oportunizando que os alunos do PIBIC participem da Reunião Anual da Sociedade Brasileira para o Progresso da Ciência (SBPC) para a apresentação de suas pesquisas!
 					<br />
 				</p>
 			</div>
 		</div>
+		
+		<!--SEMIC 2010 -->
 		<div class="item-evento-anterior">
 			<div class="foto-evento-anterior">
-				<div class="semic-2010 round-foto"></div>
+				<a href="http://www2.pucpr.br/reol/index.php/semic18" target="_blank">
+					<div class="semic-2010 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/index.php/semic18" blank="_blank"><h3>2010 - XVIII Seminário de Iniciação Científica e a XII Mostra de Pesquisa (26 e 27 de outubro de 2010)</h3></a>
+				<a href="http://www2.pucpr.br/reol/index.php/semic18" target="_blank"><h3>2010 - XVIII Seminário de Iniciação Científica e a XII Mostra de Pesquisa (26 e 27 de outubro de 2010)</h3></a>
 				<p>
 					O XVIII Seminário de Iniciação Científica e a XII Mostra de Pesquisa da Pontifícia Universidade Católica do Paraná é o maior evento de pesquisa da universidade, que congrega a pesquisa nos diversos níveis, desde o ensino médio até o doutorado. Neste ano, o evento totaliza 524 trabalhos em todas as áreas do conhecimento, sendo 28 PIBIC Jr, 360 PIBIC e 136 pesquisas de mestrado e doutorado. O excelente desempenho dos alunos, alicerçado na qualidade do corpo de orientadores, garante o sucesso desta conquista!
 				</p>
 			</div>
 		</div>
+		
+		<!--SEMIC 2009 -->
 		<div class="item-evento-anterior">
 			<div class="foto-evento-anterior">
-				<div class="semic-2009 round-foto"></div>
+				<a href="http://www2.pucpr.br/reol/index.php/semic17" target="_blank">
+					<div class="semic-2009 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/index.php/semic17" blank="_blank"><h3>2009 - XVII Seminário de Iniciação Científica e a XI Mostra de Pesquisa (27 e 28 de outubro de 2009)</h3></a>
+				<a href="http://www2.pucpr.br/reol/index.php/semic17" target="_blank"><h3>2009 - XVII Seminário de Iniciação Científica e a XI Mostra de Pesquisa (27 e 28 de outubro de 2009)</h3></a>
 				<p>
 					O objetivo maior dos Programas de Iniciação Científica - PIBIC e PIBIC Jr - é a formação de recursos humanos por meio do aprendizado e desenvolvimento de atividades de pesquisa sob a orientação de um professor pesquisador. A Pontifícia Universidade Católica do Paraná tem dado grande atenção à formação do jovem pesquisador e já possui uma longa tradição na Iniciação Científica, participando do PIBIC há18 anos, do PIBIC Jr há 3 anos. Além disso, é a única IES que oferece o PIBIC Jr para colégios privados como contrapartida às bolsas recebidas do CNPq.
 				</p>
 			</div>
 		</div>
+		
+		<!--SEMIC 2008 -->
 		<div class="item-evento-anterior">
 			<div class="foto-evento-anterior">
-				<div class="semic-2008 round-foto"></div>
+				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2008?dd99=" target="_blank">
+					<div class="semic-2008 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2008?dd99=" blank="_blank"><h3>2008 - XVI Seminário de Iniciação Científica e a X Mostra de Pesquisa (11 e 12 de novembro de 2008)</h3></a>
+				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2008?dd99=" target="_blank"><h3>2008 - XVI Seminário de Iniciação Científica e a X Mostra de Pesquisa (11 e 12 de novembro de 2008)</h3></a>
 				<p>
 					O XVI Seminário de Iniciação Científica e a X Mostra de Pesquisa da PUCPR representam um resumo de toda a geração de conhecimento técnico e científico da universidade, realizada por alunos de Iniciação Científica, oportunizando a eles, um desenvolvimento pessoal e maturidade profissional que certamente representarão grande diferencial de competitividade na acirrada disputa pelo espaço profissional.
 				</p>
 			</div>
 		</div>
+		
+		<!--SEMIC 2007 -->
 		<div class="item-evento-anterior">
 			<div class="foto-evento-anterior">
-				<div class="semic-2007 round-foto"></div>
+				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2007?dd99=" target="_blank">
+					<div class="semic-2007 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2007?dd99=" blank="_blank"><h3>2007 - XV Seminário de Iniciação Científica e a IX Mostra de Pesquisa da PUCPR (30 e 31 de outubro de 2007)</h3></a>
+				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2007?dd99=" target="_blank"><h3>2007 - XV Seminário de Iniciação Científica e a IX Mostra de Pesquisa da PUCPR (30 e 31 de outubro de 2007)</h3></a>
 				<p>
 					A Pontifícia Universidade Católica do Paraná tem na Iniciação Científica uma substancial contribuição para a formação do ser humano em todas as suas dimensões, não somente nas intelectuais. Assim, como resultado da sua priorização institucional, o PIBIC tem sido consolidado e expandido, neste ano temos a primeira edição do Seminário de Pesquisa Júnior, com a participação de alunos do ensino médio da rede pública e privada. O XV Seminário de Iniciação Científica e a IX Mostra de Pesquisa são uma demonstração da maturidade científica da PUCPR, à qual se soma seus 12 cursos de mestrado stricto sensu e sete de doutorado, envolvendo todas as áreas do conhecimento.
 				</p>
 			</div>
 		</div>
+		
+		<!--SEMIC 2006 -->
 		<div class="item-evento-anterior">
 			<div class="foto-evento-anterior">
-				<div class="semic-2006 round-foto"></div>
+				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2006?dd99=" target="_blank">
+					<div class="semic-2006 round-foto"></div>
+				</a>
 			</div>
 			<div class="descricao-evento-anterior">
-				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2006" blank="_blank"><h3>2006 - XIV Seminário de Iniciação Científica e a VIII Mostra de Pesquisa (24 e 25 de outubro de 2006)</h3></a>
+				<a href="http://www2.pucpr.br/reol/index.php/PIBIC2006?dd99=" target="_blank"><h3>2006 - XIV Seminário de Iniciação Científica e a VIII Mostra de Pesquisa (24 e 25 de outubro de 2006)</h3></a>
 				<p>
 					O modelo de apoio governamental à iniciação científica, através do Programa Institucional de Bolsas de Iniciação Científica (PIBIC), criado pelo CNPq, é um caso de sucesso na instrumentalização do jovem para participar na geração de conhecimento. Através da interação do aluno de maneira próxima com um professor orientador, o aluno de graduação tem a oportunidade de conhecer a aplicar o método científico à sua área de conhecimento. Como consequência, passa por um processo de verdadeira emancipação educacional para a cidadania, enquanto interage com os atores do processo (professores, alunos de pós-graduação e técnicos) e com o objeto da pesquisa, desenvolvendo habilidades como a raciocínio lógico, iniciativa, resolução de conflitos e perseverança.
 				</p>
 			</div>
 		</div>
+		
 	</div>
 </div>
