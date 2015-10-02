f674eac1fb0c3a4619aed610e9919968e9c48ffb
Elizandro Santos de Lima <santos.elizandro@pucpr.br>
2015-10-02 15:36:34 -0300

    At.SEMIC.02.10.2015.
    Documento e paginas em ingles

--- semic/system/application/views/semic2015/expedient.php
+++ semic/system/application/views/semic2015/expedient.php
@@ -1,149 +1,43 @@
 <hr size="5" width="100%" align="left" noshade>
-<h1>EXPEDIENTE</h1>
+<h1>EXPEDIENT</h1>
 <div >
 	<h2><strong> PONTIFÍCIA UNIVERSIDADE CATÓLICA DO PARANÁ - PUCPR</strong></h2>
 	<p align="justify">
-		<strong> Administração Central </strong>
-		<br /> 
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Grão-Chanceler: Dom Rafael Biernaski
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Presidente da Associação Paranaense de Cultura: Delcio Afonso Balestrin
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Vice-Presidente da Associação Paranaense de Cultura: Antônio Benedito de Oliveira
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Reitor: Waldemiro Gremski
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Vice-Reitor: Paulo Otávio Mussi Augusto
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Pró-Reitor Acadêmico: Vidal Martins
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Pró-Reitor de Pesquisa e Pós-Graduação: Paula Cristina Trevilatto
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Pró-Reitor Comunitário e de Extensão: José Luiz Casela
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Pró-Reitor de Administração, Planejamento e Desenvolvimento: Paulo de Paula Baptista
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Diretora de Pesquisa: Vanessa Santos Sotomaior 
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Diretor de Pós-Graduação: Nathan Mendes
-			<br />&nbsp;&nbsp;&nbsp;&nbsp;Coordenação da Iniciação Científica: Cleybe Hiole Vieira
-	</p>
-	
-	<p align="justify">
-		<strong> Comitê Gestor </strong>
-			<br /> 
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Cleybe Hiole Vieira 
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Adalgiza Aparecida de Oliveira Gonçalves                                    
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Amélia do Carmo Sampaio Rossi
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Angela Cristiane Santos Póvoa
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Cristina Baena 
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Emerson Paraiso
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Evelyn de Almeida Orlando
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Everdan Carneiro
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Gezelda Moraes
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Jaime Ramos
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Júlio César Nievola
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Paulo Parreira                              
-		<br />	&nbsp;&nbsp;&nbsp;&nbsp;Ruy Inacio de Carvalho
-	</p>
-	
-	<strong> Comitê Local</strong>
-	<p align="justify">
-		<strong>Ciências da Vida</strong>
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Almir Petersen Barreto
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Ana Maria Trindade Gregio 
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Andréa Novais Moreno
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Beatriz Helena Sottile França 
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Carlos Alberto Mayora Aita
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Cristina Baena 
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Edvaldo Antônio Ribeiro Rosa
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Janete Dubiaski da Silva
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;José Rocha Faria Neto 
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Paula Cristina Trevilatto
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Paulo Henrique Couto
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Roberto Pecoits Filho
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Rodrigo Rached
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Salmo Raskin
-</p>
+		<strong> Scientific Commission </strong><br /><br />
+		<table>																								
+			<tr>	<td>	Paula C. Trevilatto	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Research and Graduation Provost	</i>	</small>	</td>	</tr>	
+			<tr>	<td>	Vanessa Sotomaior 	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Research Director	</i>	</small>	</td>	</tr>	
+			<tr>	<td>	Cleybe Vieira	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Undergraduate Research Mentorship Coordinator	</i>	</small>	</td>	</tr>	
+			<tr>	<td>	Cristiane Regina Arns 	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Science and Transcendence Program 	</i>	</small>	</td>	</tr>	
+			<tr>	<td>	Bárbara Pimpão Ferreira 	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Marist Solidarity Network	</i>	</small>	</td>	</tr>	
+			<tr>	<td>	Jimena Djauara N. C. Grignani 	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Marist Solidarity Network	</i>	</small>	</td>	</tr>	
+			<tr>	<td>	Fabiano Incerti	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Science and Faith Institute	</i>	</small>	</td>	</tr>	
+			<tr>	<td>	Maria Cecília Pilla	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Human Rights Graduate Program	</i>	</small>	</td>	</tr>	
+			<tr>	<td>	Jucimeri Isolda Silveira	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Human Rights Nucleus Coordinator 	</i>	</small>	</td>	
+			</tr>
+			<td> <strong> Cultural Commission </strong> </td>	
+			<tr>	<td>	Jean Michel Silva	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Science and Faith Institute	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Cláudia Pacheco	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Science and Faith Institute	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Armando Célia Júnior	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Cultural Directorship	</i>	</small>	</td>	</tr>
+			</tr>
+			<td> <strong> Organizing Commission </strong> </td>	
+			<tr>	<td>	Vanessa Sotomaior 	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Research Director	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Cleybe Vieira	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Undergraduate Research Mentorship Coordinator	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Rene Faustino	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Knowledge Management Coordinator 	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Silvana Taschek Hastreiter	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Relationship Director 	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Thais Lúcia Costa	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Relationship Director	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Pollyanna Vanessa Mantovanello	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Relationship Director	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Jonathan Zanon Linhares 	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Relationship Director	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Alessandra Lacerda de Carvalho	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Undergraduate Research Mentorship Coordination	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Mariani Pimenta Barbosa	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Undergraduate Research Mentorship Coordination	</i>	</small>	</td>	</tr>
+			<tr>	<td>	Edena Maria Beiga Grein	</td>	<td>	<small>	<i>	&nbsp;&nbsp;&nbsp;&nbsp;	Undergraduate Research Mentorship Coordination	</i>	</small>	</td>	</tr>
 
-	<p align="justify">
-		<strong>Ciências Agronmicas E Vêterinárias</strong>
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Cláudia Turra Pimpão
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Cristina Santos Sotomaior
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Fernando Luciano Bittencourt
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Renata Ernlund Freitas de Macedo
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Ruy Inácio Neiva de Carvalho
-	</p>
-	
-	
-		<p align="justify">
-		<strong>Ciências Exatas e da Terra</strong>
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Cinthia Obladen de Almendra Freitas
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Emerson Paraiso
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Fabrício Enembreck
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp; João Elias Abdalla Filho
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Júlio César Nievola
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Leandro dos Santos Coelho
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Luciano Antonio Mendes
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Luís Mauro Moura
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Luiz Augusto de Paula Lima Júnior
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Marcelo Rudek
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Munir Antônio Gariba
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Nilson Barbieri
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Sérgio Eduardo Gouvea da Costa
-			<br /> &nbsp;&nbsp;&nbsp;&nbsp;Viviana Cocco Mariani
-	</p>
-	
-	
-		<p align="justify">
-		<strong>Ciências Humanas</strong>
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;César Candiotto
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Clélia Peretti
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Evelyn de Almeida Orlando
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Dilmeire Sant’anna Ramos Vosgerau
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Joana Paulin Romanowski
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Romilda Teodora Ens	
-	</p>
-	
-	
-		<p align="justify">
-		<strong>Ciências Sociais Aplicadas</strong>
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Angela Cristiane Santos Póvoa
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Cláudia Maria Barbosa
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Clóvis Ultramari 
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Danielle Anne Pamplona
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Harry Alberto Bollmann
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Heitor Takashi Kato
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Katya Kozicki
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Samira Kauchakje
-	</p>
-	
-	
-		<p align="justify">
-		<strong>Versão eletrônica</strong>
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Alessandra de Lacerda Carvalho
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Edena Maria Beiga Grein
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Mariani Barbosa Pimenta
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Rene Faustino Gabriel Júnior
-	</p>
-	
-	
-		<p align="justify">
-		<strong>Revisão (Português)</strong>
-		<br /> 
-	
-	</p>
 
-		<p align="justify">
-		<strong>Tradução e Revisão (Inglês) </strong>
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Carmen Terezinha Koppe
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Elis Carrijo Guimarães Mendonça
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Gisele Rietow
-		<br /> &nbsp;&nbsp;&nbsp;&nbsp;Karina Aires Reinlen Fernandes Couto de Moraes
+
+
+</tr>	</table>
 	</p>
 	
-			<p align="justify">
-		<strong>Capa</strong>
-		<br /> 
-	
-	</p>
-	
-			<p align="justify">
-		<strong>Projeto gráfico e diagramação</strong>
-		<br /> 
-	
-	</p>
-		
 </div>
 <br />
 	<hr size="5" width="100%" align="left" noshade>
